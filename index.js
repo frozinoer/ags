328c9be542024a5c7386fcbd23f587785cbd75f1
Fabrice Rozinoer <fabrice.rozinoer@Ronnys-MacBook-Pro.local>
2017-10-02 20:20:57 +0200

    removed debugs

--- index.js
+++ index.js
@@ -97,9 +97,6 @@ const getNewTravelers = (users) => {
         m.add(user.searchParams.days, 'days');
         let departureText = m.format("YYYY-MM-DD");
 
-        console.log("days: " + user.searchParams.days);
-        console.log("departureText: " + departureText);
-
         user.searchParams.arrivalDate = arrivalText;
         user.searchParams.departureDate = departureText;
 
